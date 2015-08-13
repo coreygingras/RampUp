12dc14d6a409e9bbacb148531504f4d26600b2b3
Corey Gingras <gingras.corey@gmail.com>
2015-08-13 19:23:21 -0400

    fizzbuzz.rb

---
+++ fizzbuzz.rb
@@ -0,0 +1,16 @@
+
+count = 0
+while count < 100
+	count += 1
+	if count % 15 == 0 
+		puts "fizzbuzz"
+	elsif count % 3 == 0
+		puts "fizz"
+	elsif count % 5 == 0
+		puts "buzz"
+	else puts count	
+	end
+end
+	
+
+
